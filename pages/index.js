2616a206a5535d3f9846edde723a22e4a015e9f9
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2020-07-23 12:19:45 +0530

    step 6: change default port to 4000

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,12 @@ import axios from 'axios';
 
 function Home({ products }) {
   console.log(products)
-  // React.useEffect(() => {
-  //   getProducts();
-  // }, []);
-
-  // async function getProducts() {
-  //   const url = 'http://localhost:3000/api/products'
-  //   const response = await axios.get(url);
-  //   console.log(response.data);
-  // }
-
   return <>home</>;
 }
 
 Home.getInitialProps = async () => {
   // fetch data on server
-  const url = 'http://localhost:3000/api/products'
+  const url = 'http://localhost:4000/api/products'
   const response = await axios.get(url);
   // return response data as an object
   return { products: response.data }
