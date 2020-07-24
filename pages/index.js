48cd99dfb326c759bffcbaf116565c757927c205
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2020-07-24 16:07:48 +0530

    step 9: tie products list ui on products component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
 
 function Home({ products }) {
-  console.log(products)
-  return <>home</>;
+  return <ProductList products={products}/>
 }
 
 Home.getInitialProps = async () => {
