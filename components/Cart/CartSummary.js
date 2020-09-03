5d495de9297e52c40b0bad232e8d575f03cffd5f
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2020-09-03 07:59:36 +0530

    Step 16: cart ui

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,18 @@
+import { Button, Segment, Divider } from 'semantic-ui-react';
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return <>
+    <Divider/>
+    <Segment clearing size="large">
+      <strong>Sub total:</strong> $0.0
+      <Button 
+        icon="cart"
+        color="teal"
+        floated="right"
+        content="Checkout"
+      />
+    </Segment>
+  </>;
 }
 
 export default CartSummary;
