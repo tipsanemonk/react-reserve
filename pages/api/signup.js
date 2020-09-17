c06ea691053c172b44a24bbe5d6d39dcaf4857cd
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2020-09-17 13:22:43 +0530

    signup validation server

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,18 +2,29 @@ import connectDb from '../../utils/connectDb';
 import User from '../../models/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
 
 connectDb();
 
 export default async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    // 0. validation
+    if (!isLength(name, { min: 3, max: 10})) { 
+      return res.status(422).send('Name must be 3-10 characters');
+    } else if (!isLength(password, { min: 6})) {
+      return res.status(422).send('Password must be at least 6 characters');
+    } else if (!isEmail(email)) {
+      return res.status(422).send('Email must be valid');
+    }
+
     // 1. check if the user already exists in the db
     const user = await User.findOne({ email })
     if (user) {
       return res.status(422).send(`User already exists with email ${email}`);
     }
-    
+
     // 2. if not, has their password
     const hash = await bcrypt.hash(password, 10);
 
