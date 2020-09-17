c5251723fe2f16dfb918d43c136215025fb3356f
Karthik Rana <karthik.rana@kdi.kongsberg.com>
2020-09-17 12:09:26 +0530

    Sign up post and auth token generation with handling of login

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://tipsaneDB:tipsaneDB@cluster0.xuwwv.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "orland",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/tipsane/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
--- pages/signup.js
+++ pages/signup.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
 import Link from 'next/link';
 import catchErrors from '../utils/catchErrors';
+import { handleLogin } from '../utils/auth';
+import baseUrl from "../utils/baseUrl";
+import axios from 'axios';
 
 const INITIAL_USER = {
   name: '',
@@ -29,8 +32,10 @@ function Signup() {
     try {
       setLoading(true);
       setError('');
-      console.log(user);
-      // TODO: make request
+      const url = `${baseUrl}/api/signup`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError)
     } finally {
@@ -75,6 +80,7 @@ function Signup() {
         />
         <Form.Input 
           fluid
+          type="password"
           icon="lock"
           iconPosition="left"
           label="Password"
